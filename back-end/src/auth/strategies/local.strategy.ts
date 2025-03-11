ed3dd71601d3a0e40495575729100e60740de5de
carloshenryck <ryck302@gmail.com>
2025-03-11 19:07:21 -0300

    feat: validate login data

--- back-end/src/auth/strategies/local.strategy.ts
+++ back-end/src/auth/strategies/local.strategy.ts
@@ -2,6 +2,9 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { LoginDto } from '../dto/login.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,6 +13,12 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string) {
+    const loginDtoInstance = plainToInstance(LoginDto, { email, password });
+    const errors = await validate(loginDtoInstance);
+    if (errors.length) {
+      throw new UnauthorizedException('Email ou senha incorretos');
+    }
+
     const user = await this.authService.validateUser(email, password);
     if (!user) {
       throw new UnauthorizedException('Email ou senha incorretos');

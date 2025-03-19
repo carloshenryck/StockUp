d1bd57f1006c2088f7a98fbd5d6e8cc531a21138
carloshenryck <ryck302@gmail.com>
2025-03-19 15:55:23 -0300

    feat: add company table

---
+++ back-end/prisma/migrations/20250319181924_add_company/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `refresh_token` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "refresh_token",
+ADD COLUMN     "companyId" TEXT,
+ADD COLUMN     "refreshToken" TEXT;
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;

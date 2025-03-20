188602532a2b7073daf35a3e3c0b5d3a7ff835e0
carloshenryck <ryck302@gmail.com>
2025-03-20 16:14:10 -0300

    refactor: change jwt strategy to get jwt from cookie

--- back-end/src/auth/strategies/jwt.strategy.ts
+++ back-end/src/auth/strategies/jwt.strategy.ts
@@ -3,12 +3,18 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from '../types/TokenPayload';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const cookies = request.cookies as { [key: string]: string };
+          return cookies?.accessToken;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: configService.get('JWT_TOKEN_SECRET') as string,
     });
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,9 +5,9 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
